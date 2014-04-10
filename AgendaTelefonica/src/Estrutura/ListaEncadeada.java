c23f7b0c5fb00fbc8f89aed3d15fec16017966fd
MichelFF <shelfagundes@hotmail.com>
2014-04-10 16:41:01 -0300

    Create ListaEncadeada.java
    classe ListaEncadeada

---
+++ AgendaTelefonica/src/Estrutura/ListaEncadeada.java
@@ -0,0 +1,102 @@
+
+package Estrutura;
+
+import static java.lang.System.out;
+
+
+
+public class ListaEncadeada<T extends Comparable<T>> {
+
+	private Nodo<T> head; // sera o Nodo
+	private Nodo<T> tail; //    Nodo
+	public  int upDown = 0;
+
+	
+	public Nodo<T> getHead()
+	{
+		return head;
+	}
+
+	public void print()
+	{
+		Nodo<?> nodo = head;
+		
+		do {
+		    String temp1 = nodo.getData().toString();
+			
+		    out.println(temp1);
+			nodo = nodo.getNext();
+			
+		} while (nodo != null);
+	}
+
+	//mostrando por caracteres
+	
+	public void printUniqueChar(char character)
+	{
+	    String letra = Character.toString(character).toLowerCase();
+	    char c = letra.charAt(0);
+	    
+		Nodo<?> nodo = head;
+		do {
+		    String temp = nodo.getData().toString();
+		    if(temp.charAt(0) == c){
+			out.println(nodo.getData());
+			
+		    }
+		    nodo = nodo.getNext();
+		} while (nodo != null);
+	}
+
+
+	public void insert(Nodo<T> novo)
+	{
+		novo.setNext(head);
+		head = novo;
+
+		if (tail == null)
+			tail = head;
+	}
+
+	public void insert(Nodo<T> novo, Nodo<T> anterior)
+	{
+		if (anterior == tail) {
+			tail.setNext((Nodo<T>)novo);
+			   tail = novo;
+			   
+			   
+		} else {
+			   novo.setNext(anterior.getNext());
+			      anterior.setNext(novo);
+		}
+	}
+	
+	
+	
+
+	public void append(Nodo<T> novo)
+	   {
+		  tail.setNext(novo);
+		       tail = novo;
+	}
+
+	
+	public void printAgain()
+	{
+		Nodo<?> nodo = head;
+	
+		    String temp1;
+		
+		 int i = 0;
+		   do{
+		     temp1 = nodo.getData().toString();
+			     nodo = nodo.getNext();
+					
+			i++;
+			
+		 }while(i<upDown && nodo != null);
+			
+		
+		 out.println(temp1);
+	}
+}
