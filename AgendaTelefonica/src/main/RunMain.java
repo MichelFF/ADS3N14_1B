75617c43f40e9ece65b3622e0ee75baaf71dfbb3
MichelFF <shelfagundes@hotmail.com>
2014-04-10 16:49:28 -0300

    Create RunMain.java

---
+++ AgendaTelefonica/src/main/RunMain.java
@@ -0,0 +1,41 @@
+
+package main;
+
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+     public class RunMain {
+
+	
+
+	public static void main(String [] args) throws IOException{
+
+	  
+	    
+	    while(true){
+		  
+	        Comandos prompt = new Comandos();
+	    
+	             prompt.showMenu();
+	    
+	                 Integer Entrada;
+	                 
+	                 
+	                 
+	                 
+	      Comandos.sc = new Scanner(System.in);
+	    
+	             Entrada = Comandos.sc.nextInt();
+	    
+
+	                   prompt.readAction(Entrada);
+	    
+	}   
+		
+	
+	
+	}
+
+}
