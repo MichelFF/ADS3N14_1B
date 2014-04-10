2e6267f1106c8c4e9896922c0f7f428f8fb8908d
MichelFF <shelfagundes@hotmail.com>
2014-04-10 16:43:56 -0300

    Create Pessoa.java
    Classe Pessoa

---
+++ AgendaTelefonica/src/Estrutura/Pessoa.java
@@ -0,0 +1,32 @@
+
+package Estrutura;
+
+
+public class Pessoa {
+	
+	private String nome;
+	   private String telefone;
+
+	public String getNome() //nome do contato na agenda telefonica
+	  {
+		return nome;
+	      }
+
+	public void setNome(String nome) 
+	    
+	  {
+		this.nome = nome;
+	       }
+
+	
+	public String getTelefone() //numero do telefone do contato
+	   {
+		return telefone;
+	        }
+
+	public void setTelefone(String telefone) 
+	      
+	   {
+		this.telefone = telefone;
+	         }
+}
