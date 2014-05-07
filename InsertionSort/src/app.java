945e8f5ba5570ca9fcdab80c5a233660aeee7570
MichelFF <shelfagundes@hotmail.com>
2014-05-07 14:26:05 -0300

    Create app.java
    app.java

---
+++ InsertionSort/src/app.java
@@ -0,0 +1,16 @@
+
+public class app {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		InsertionClass obj1 = new InsertionClass();
+		
+		obj1.teste();
+		// TODO Auto-generated method stub
+
+	}
+
+}
